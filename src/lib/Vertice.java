fe676fc75dc02557ca28d1878d2a0254caf39463
Thalison Vinicius <69252647+Thalison17@users.noreply.github.com>
2024-08-12 20:07:53 -0300

    Fim commit modularização

---
+++ src/lib/Vertice.java
@@ -0,0 +1,19 @@
+package lib;
+
+public class Vertice<T> {
+
+    private T valor;
+
+    public Vertice(T valor) {
+        this.valor = valor;
+    }
+
+    public T getValor() {
+        return valor;
+    }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+}
